80004f89ce7901deee1de69be57ef805881cd577
Mancipox <smancipeparedes@gmail.com>
2019-04-05 22:08:33 -0500

    Added NavigationDrawer

--- app/src/main/java/com/tambo/Connection/Connect_Server.java
+++ app/src/main/java/com/tambo/Connection/Connect_Server.java
@@ -3,5 +3,5 @@ package com.tambo.Connection;
 
 public interface Connect_Server {
 
-    String url_server = "http://192.168.137.1:8080/ServerTambo/";
+    String url_server = "http://192.168.0.30:8080/ServerTambo/";
 }
--- app/src/main/java/com/tambo/Controller/YekabeActivity.java
+++ app/src/main/java/com/tambo/Controller/YekabeActivity.java
@@ -2,13 +2,21 @@ package com.tambo.Controller;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.tambo.LocalCommunication.DataCommunication;
 import com.tambo.Model.Question;
@@ -22,31 +30,27 @@ import java.util.ArrayList;
  * Main activity of student-professor. Implements methods of {@link DataCommunication}
  */
 public class YekabeActivity extends AppCompatActivity implements DataCommunication {
-    private Toolbar toolbar;
+
     private TabLayout tabLayout;
     private TabItem tabItemStudent;
     private TabItem tabItemProfessor;
-    private ViewPager viewPager;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mDrawerView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     private String questionText;
-    private EditText questionEditText;
-    private boolean sucefullPost;
-
-    private AdapterQuestionStudent adapterQuestionStudent;
 
     private User user;
-
-    private ArrayList<Question> questionsStudents;
-    private ArrayList<Question> questionsProfessor;
-
 
     private Question questionProfessor;
     private Question questionStudent;
 
     private String token;
 
+
+
     /**
-     * Set the view @BD
+     * Set the view
      * @param savedInstanceState
      */
     @Override
@@ -55,15 +59,41 @@ public class YekabeActivity extends AppCompatActivity implements DataCommunicati
 
         setContentView(R.layout.activity_yekabe);
 
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle(getResources().getString(R.string.app_name));
         tabLayout = findViewById(R.id.tablayout);
         tabItemStudent = findViewById(R.id.tabStudent);
         tabItemProfessor = findViewById(R.id.tabProfessor);
 
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_nav,R.string.close_nav);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerView = findViewById(R.id.navigation_view);
+
+        mDrawerView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch(id){
+                    case R.id.account:
+                        Toast.makeText(YekabeActivity.this, "My account", Toast.LENGTH_SHORT).show(); break;
+                    case R.id.settings:
+                        Toast.makeText(YekabeActivity.this, "Settings", Toast.LENGTH_SHORT).show(); break;
+                    case R.id.mycart:
+                        Toast.makeText(YekabeActivity.this, "My cart", Toast.LENGTH_SHORT).show(); break;
+                        default: return true;
+                }
+                return true;
+            }
+        });
+
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+        //tabLayout.setupWithViewPager(viewPager);
+
+        final PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pageAdapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
@@ -124,5 +154,11 @@ public class YekabeActivity extends AppCompatActivity implements DataCommunicati
         this.token=token;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
 
+        return super.onOptionsItemSelected(item);
+    }
 }
