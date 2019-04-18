3a4ca911fc5110aad15cc05799cf25d1927bd325
Steve <myjachis@gmail.com>
2019-04-18 10:53:57 -0500

    Added a new activity that allows the user to edit its personal data

--- app/src/main/java/com/tambo/Controller/MyProfileActivity.java
+++ app/src/main/java/com/tambo/Controller/MyProfileActivity.java
@@ -1,7 +1,10 @@
 package com.tambo.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,6 +13,7 @@ import com.tambo.R;
 
 public class MyProfileActivity extends AppCompatActivity {
     private TextView textView;
+    private Button editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class MyProfileActivity extends AppCompatActivity {
         final Bundle extras = getIntent().getExtras();
         final User usermain = (User)extras.get("user");
         final String sendtoken = (String) extras.get("token");
+        editButton=findViewById(R.id.edit_button);
 
         textView= findViewById(R.id.profile_username);
         textView.setText(usermain.getUserName());
@@ -27,17 +32,25 @@ public class MyProfileActivity extends AppCompatActivity {
         textView.setText(usermain.getLastName());
         TextView textViewKarma= findViewById(R.id.karma_number);
         textViewKarma.setText(String.valueOf(usermain.getKarma()));
-        Toast.makeText(MyProfileActivity.this, "Mi perfil "+usermain.getKarma(), Toast.LENGTH_SHORT).show();
+    //    Toast.makeText(MyProfileActivity.this, "Mi perfil "+usermain.getKarma(), Toast.LENGTH_SHORT).show();
         textView= findViewById(R.id.profile_email);
         textView.setText(usermain.getEmail());
         textView= findViewById(R.id.profile_number);
         textView.setText(usermain.getPhone());
-        /*
-        textView.setText(usermain.getKarma());
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyProfileActivity.this, EditProfileActivity.class);
+                intent.putExtra("user", usermain);
+                intent.putExtra("token",sendtoken);
+                startActivity(intent);
+
+            }
 
 
-        */
-
-
+    });
     }
 }
+
